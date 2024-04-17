0fe0c54448b27f13dd77b244ec39b90419055d04
Carlos Silva <carlos.silva@fortics.com.br>
2024-04-17 14:31:04 -0300

    loucuras

--- app.py
+++ app.py
@@ -35,12 +35,12 @@ def auth():
         
         return authenticator
     
-#    
-#@st.cache_data(show_spinner=True)
-#def start_flask_app():
-#    flask_script_path = "mailer/teste.py"
-#    print('iniciando flask')
-#    subprocess.Popen(["python", flask_script_path])
+    
+@st.cache_data(show_spinner=True)
+def start_flask_app():
+    flask_script_path = "mailer/teste.py"
+    print('iniciando flask')
+    subprocess.Popen(["python", flask_script_path])
     
 @st.cache_data(show_spinner=True)
 def custom_css():
@@ -68,55 +68,7 @@ def custom_css():
 st.set_page_config(
     page_title="RoomSense - Home",
     page_icon=":calendar:",
-    initial_sidebar_state="expanded")
-
-
-from flask import Flask, render_template, request
-import sqlite3
-
-app = Flask(__name__)
-
-
-
-def get_db_connection():
-    
-    conn = sqlite3.connect('mailer/respostas.db')
-    
-    conn.row_factory = sqlite3.Row
-    
-    return conn
-
-
-@app.route('/confirmacaoA/<string:email>/<string:resposta>/<string:data>', methods=['GET'])
-def confirmacao(email, resposta,data):
-    
-    inserir_resposta_no_banco(email, resposta,data)
-    
-    return render_template('index.html')
-
-
-@app.route('/confirmacaoB/<string:email>/<string:resposta>/<string:data>', methods=['GET'])
-def confirmacaob(email, resposta,data):
-    
-    inserir_resposta_no_banco(email, resposta,data)
-        
-    return render_template('index2.html')
-
-
-def inserir_resposta_no_banco(email, resposta,data):
-    
-    conn = get_db_connection()
-    
-    conn.execute("INSERT INTO respostas (email, resposta,data) VALUES (?,?,?)", (email, resposta,data))
-    
-    conn.commit()
-    
-    conn.close()
-    
-
-
-app.run(debug=True, host='0.0.0.0')
-            
+    initial_sidebar_state="expanded")            
     
 config = yamlconfig()
 
